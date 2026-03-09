5d64bd2111c9fa95a91d7c9e49021ba98aa377f3
Somya-Pancholi <asomiy100@gmail.com>
2026-03-09 15:34:18 +0530

    added resume template and brochure

--- src/constants/internshipsConstants.js
+++ src/constants/internshipsConstants.js
@@ -12,6 +12,13 @@ export const ACTION_BUTTONS = [
   /*{ label: 'REGISTER AS COMPANY', type: 'gradient' }*/
 ];
 
+// filename of the brochure PDF located in src/assets
+export const BROCHURE_FILE_NAME = 'Company Brochure Internship Fair.pdf';
+
+// Overleaf URL for Jake's resume template. Update this to the real link.
+export const RESUME_TEMPLATE_URL = 'https://www.overleaf.com/latex/templates/jakes-resume-anonymous/cstpnrbkhndn/';
+
+
 export const STATS = [
   { value: '10', label: 'Companies' },
   { value: '60+', label: 'Positions' },
